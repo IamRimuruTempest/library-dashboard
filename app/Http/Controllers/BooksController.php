b22d4e1422675b016acad3f94629510e32ccff2f
IamRimuruTempest <iamtrebojotneivda07@gmail.com>
2023-06-03 23:34:02 +0800

    Initial Commit

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -20,52 +20,94 @@ class BooksController extends Controller
    
 
     public function store(Request $request) {
+        // $image = null;
+
+        // $category = $request->category;
+        // $year = $request->year;
+        // $isbn = $request->isbn;
+        // $datePurchased = $request->datePurchased;
+        // $price = $request->price;
+        // $shelfNumber = $request->shelfNumber;
+        // $description = $request->description;
+
+        // if($category == 'null' || $category == 'undefined') {
+        //     $category = null;
+        // } 
+        // if($year == 'null' || $year == 'undefined') {
+        //     $year = null;
+        // } 
+        // if($isbn == 'null' || $isbn == 'undefined') {
+        //     $isbn = null;
+        // }
+        // if( $datePurchased == 'null' || $datePurchased == 'undefined') {
+        //     $datePurchased = null;
+        // }
+        // if($price == 'null' || $price == 'undefined') {
+        //     $price = null;
+        // }
+        // if($shelfNumber == 'null' || $shelfNumber == 'undefined') {
+        //     $shelfNumber = null;
+        // }
+        //  if($description == 'null' || $description == 'undefined') {
+        //     $description = null;
+        // }
+
+        // if($request->filled('image')) {
+        //     $image = null;
+        // } else {
+        //      if ($file = $request->file('image')) {
+        //         $destinationPath = 'book-cover/';
+        //         $image = $request->bookNum . "." . $file->getClientOriginalExtension();
+        //         $file->move($destinationPath, $image);
+        //         $input['image'] = "$image";
+        //     }
+        // }
+
+        // DB::connection('mysql')
+        // ->table('books')
+        // ->insert([
+        //     'book_id' => $request->bookNum,
+        //     'title' => $request->title,
+        //     'author' => $request->author,
+        //     'publisher' => $request->publisher,
+        //     'category' => $category,
+        //     'year' => $year,
+        //     'isbn' => $isbn,
+        //     'date_purchased' => $datePurchased,
+        //     'price' => $price,
+        //     'shelf_no' => $shelfNumber,
+        //     'status' => $request->status,
+        //     'book_description' => $description,
+        //     'book_image' => $image,
+        //     'created_at' => new \Datetime
+        // ]);
+
+        
+        // DB::connection('mysql')
+        // ->table('ratings')
+        // ->insert([
+        //    'isbn' => $isbn,
+        // ]);
+
         $image = null;
+        $category = $request->category ?? null;
+        $year = $request->year ?? null;
+        $isbn = $request->isbn ?? null;
+        $datePurchased = $request->datePurchased ?? null;
+        $price = $request->price ?? null;
+        $shelfNumber = $request->shelfNumber ?? null;
+        $description = $request->description ?? null;
 
-        $category = $request->category;
-        $year = $request->year;
-        $isbn = $request->isbn;
-        $datePurchased = $request->datePurchased;
-        $price = $request->price;
-        $shelfNumber = $request->shelfNumber;
-        $description = $request->description;
-
-        if($category == 'null' || $category == 'undefined') {
-            $category = null;
-        } 
-        if($year == 'null' || $year == 'undefined') {
-            $year = null;
-        } 
-        if($isbn == 'null' || $isbn == 'undefined') {
-            $isbn = null;
-        }
-        if( $datePurchased == 'null' || $datePurchased == 'undefined') {
-            $datePurchased = null;
-        }
-        if($price == 'null' || $price == 'undefined') {
-            $price = null;
-        }
-        if($shelfNumber == 'null' || $shelfNumber == 'undefined') {
-            $shelfNumber = null;
-        }
-         if($description == 'null' || $description == 'undefined') {
-            $description = null;
-        }
-
-        if($request->filled('image')) {
+        if ($request->filled('image')) {
             $image = null;
-        } else {
-             if ($file = $request->file('image')) {
-                $destinationPath = 'book-cover/';
-                $image = $request->bookNum . "." . $file->getClientOriginalExtension();
-                $file->move($destinationPath, $image);
-                $input['image'] = "$image";
-            }
+        } elseif ($file = $request->file('image')) {
+            $destinationPath = 'book-cover/';
+            $image = $request->bookNum . "." . $file->getClientOriginalExtension();
+            $file->move($destinationPath, $image);
+            $input['image'] = $image;
         }
 
-        DB::connection('mysql')
-        ->table('books')
-        ->insert([
+        DB::connection('mysql')->table('books')->insert([
             'book_id' => $request->bookNum,
             'title' => $request->title,
             'author' => $request->author,
@@ -82,11 +124,8 @@ class BooksController extends Controller
             'created_at' => new \Datetime
         ]);
 
-        
-        DB::connection('mysql')
-        ->table('ratings')
-        ->insert([
-           'isbn' => $isbn,
+        DB::connection('mysql')->table('ratings')->insert([
+            'isbn' => $isbn,
         ]);
     } 
 
